77e46299ec1f85b389dcc14bd11b7a71bc9c3e21
32blejd <152063649+32blejd@users.noreply.github.com>
2024-06-19 23:41:29 +0200

    Add files via upload

---
+++ komendyztutoriala.sh
@@ -0,0 +1,45 @@
+git commit
+git checkout -b bugFix
+git commit
+git checkout
+git checkout main
+git commit
+objective
+git merge bugfix
+git checkout -b bugFix
+git commit
+git checkout main
+git commit
+git checkout bugfix
+git rebase main
+git checkout c4
+git checkout c3
+git branch -f main C6
+git checkout HEAD~1
+git branch -f bugFix HEAD~~
+git cherry-pick c3 c4 c7
+git rebase -i c1
+git rebase -i c1
+git rebase bugFix main
+git rebase -i HEAD~2
+git commit --amend
+git rebase -i HEAD~2
+git rebase caption main
+git checkout main
+git cherry-pick C2
+git commit --amend
+git cherry-pick C3
+git tag v1 side~1
+git tag v0 main~2
+git checkout v1
+git commit
+git rebase main bugFix
+git rebase bugFix side
+git rebase side another
+git rebase another main
+git branch bugWork main^^2^
+git checkout one
+git cherry-pick C4 C3 C2
+git checkout two
+git cherry-pick C5 C4 C3 C2
+git branch -f three C2
